75a6a4b10c68b177f4dd4f434bd8a3a9e240912c
vanyadmitrievich2007-ops <vanya.dmitrievich.2007@gmail.com>
2026-06-19 02:09:19 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+document
+.getElementById("requestForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert(
+        "Спасибо! Ваша заявка успешно отправлена. Мы свяжемся с вами в ближайшее время."
+    );
+
+    this.reset();
+});
+
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    reveals.forEach((element) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (elementTop < windowHeight - revealPoint) {
+            element.classList.add('active');
+        } else {
+            element.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', reveal);reveal();
+
+document.getElementById("requestform").addEventListener("submit", function(e) 
+{ e.preventDefault();
+ const name = document.querySelector("[name='name']").value;
+ const phone = document.querySelector("[name='phone']").value;
+ const msg = document.querySelector("[name='message']").value;
+
+  const text =
+ `Здравствуйте! Новая заявка:
+ Имя: ${name}
+ Телефон: ${phone}
+ Опишите необходимую услугу: ${msg}`;
+
+  const url = "https://wa.me/79142656584?text=" + encodeURIComponent(text);
+
+ window.location.href = url;
+});
